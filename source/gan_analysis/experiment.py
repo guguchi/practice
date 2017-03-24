8eb85647017b0dfd8864a20ce4ddd147e68f08bb
guguchi <docomosoft091@gmail.com>
2017-03-25 00:09:56 +0900

    0325

--- source/gan_analysis/experiment.py
+++ source/gan_analysis/experiment.py
@@ -12,9 +12,17 @@ num_cluster_list = [8, 4, 2, 1]
 scale = 2
 std_list = [0.5, 0.4, 0.3, 0.2, 0.1]
 z_size = [500, 100, 50, 10, 2, 1]
+save_root = '/home/yamaguchi-s/Desktop/Research/practice/data/gan_analysis/20170324/'
 
 for learning_rate_D in learning_rate_D_list:
     for learning_rate_G in learning_rate_G_list:
         for num_cluster in num_cluster_list:
             for std in std_list:
-                save_path = '/home/yamaguchi-s/Desktop/Research/practice/data/gan_analysis/'
+                save_path = save_root+'d_{}_g_{}_cl_{}_std{}/'.format(
+                             learning_rate_D, learning_rate_G, num_cluster, std)
+                SLURM_commands=['python /home/yamaguchi-s/Desktop/Research/practice/source/gan_analysis/main.py {} {} {} {} {} {} {} {}'.format(
+                                save_path, step, learning_rate_D, learning_rate_G
+                                num_cluster, scale, std, z_size)]
+            	res, success=slurm_tools.slurm_submit(SLURM_commands, mem=16000, gres='gpu:1')
+
+                print "job number: "+res
