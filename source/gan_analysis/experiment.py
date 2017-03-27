7c9593ff336ca5a16b64a9f1ca7c330ecbea34fd
guguchi <docomosoft091@gmail.com>
2017-03-27 22:23:25 +0900

    0327

--- source/gan_analysis/experiment.py
+++ source/gan_analysis/experiment.py
@@ -6,14 +6,44 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
 from slurm import slurm_tools
 
 step = 1000000
-learning_rate_D_list = [0.001]#, 0.00075, 0.0005, 0.00025, 0.0001
-learning_rate_G_list = [0.001]#, 0.00075, 0.0005, 0.00025, 0.0001
+learning_rate_D_list = [0.0005, 0.0004, 0.0003, 0.0002, 0.0001, 0.00009, 0.00008, 0.00007, 0.00006, 0.00005]
+learning_rate_G_list = [0.0005, 0.0004, 0.0003, 0.0002, 0.0001, 0.00009, 0.00008, 0.00007, 0.00006, 0.00005]
 num_cluster_list = [8]#[8, 4, 2, 1]
 scale = 2
 std_list = [0.2]#[0.5, 0.4, 0.3, 0.2, 0.1]
-z_size_list = [10]#[500, 100, 50, 10, 2, 1]#,50 10
+z_size_list = [100, 50, 10, 2, 1]#[500, 100, 50, 10, 2, 1]#,50 10
 date = '20170327/'
-concept = ''
+concept = 'z_zize/'
+save_root = '/home/yamaguchi-s/Desktop/Research/practice/data/gan_analysis/'+date+concept
+
+for learning_rate_D in learning_rate_D_list:
+    for learning_rate_G in learning_rate_G_list:
+        for num_cluster in num_cluster_list:
+            for std in std_list:
+                for z_size in z_size_list:
+
+                    save_path = save_root+'d_{}_g_{}_cl_{}_std_{}_z_{}/'.format(
+                     learning_rate_D, learning_rate_G, num_cluster, std, z_size)
+
+                    SLURM_commands=['python /home/yamaguchi-s/Desktop/Research/practice/source/gan_analysis/main.py {} {} {} {} {} {} {} {}'.format(
+                                save_path, step, learning_rate_D, learning_rate_G,
+                                num_cluster, scale, std, z_size)]
+
+            	    res, success=slurm_tools.slurm_submit(SLURM_commands, mem=16000, gres='gpu:1')
+
+                    print success
+                    print res
+
+
+step = 1000000
+learning_rate_D_list = [0.0005, 0.0004, 0.0003, 0.0002, 0.0001, 0.00009, 0.00008, 0.00007, 0.00006, 0.00005]
+learning_rate_G_list = [0.0005, 0.0004, 0.0003, 0.0002, 0.0001, 0.00009, 0.00008, 0.00007, 0.00006, 0.00005]
+num_cluster_list = [8, 4, 2, 1]#[8, 4, 2, 1]
+scale = 2
+std_list = [0.2]#[0.5, 0.4, 0.3, 0.2, 0.1]
+z_size_list = [50]#[500, 100, 50, 10, 2, 1]#,50 10
+date = '20170327/'
+concept = 'num_cluster/'
 save_root = '/home/yamaguchi-s/Desktop/Research/practice/data/gan_analysis/'+date+concept
 
 for learning_rate_D in learning_rate_D_list:
