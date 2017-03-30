047415e0b3688c172b8e133269ade26107c028ef
guguchi <docomosoft091@gmail.com>
2017-03-31 00:02:33 +0900

    0330

--- source/gan_analysis/experiment_vanilla.py
+++ source/gan_analysis/experiment_vanilla.py
@@ -35,8 +35,5 @@ for learning_rate_D in learning_rate_D_list:
 
                 res, success=slurm_tools.slurm_submit(SLURM_commands, mem=16000, gres='gpu:1')
 
-                print res
                 print success
-
-
-                    print res
+                print res
