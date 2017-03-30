5eee8347a85bc7b4942b08e4874e3c94eb862f26
guguchi <docomosoft091@gmail.com>
2017-03-30 09:59:30 +0900

    0330

--- source/gan_analysis/experiment.py
+++ source/gan_analysis/experiment.py
@@ -4,7 +4,7 @@
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
 from slurm import slurm_tools
-
+"""
 step = 300000
 learning_rate_D_list = [0.0001, 0.00005, 0.00001]
 learning_rate_G_list = [0.0001, 0.00005, 0.00001]
@@ -38,7 +38,7 @@ for learning_rate_D in learning_rate_D_list:
 
                     print success
                     print res
-
+"""
 step = 300000
 learning_rate_D_list = [0.0001, 0.00005, 0.00001]
 learning_rate_G_list = [0.0001, 0.00005, 0.00001]
@@ -64,7 +64,7 @@ for learning_rate_D in learning_rate_D_list:
                     save_fig_path = save_fig_root+'d_{}_g_{}_cl_{}_std_{}_z_{}/'.format(
                       learning_rate_D, learning_rate_G, num_cluster, std, z_size)
 
-                    SLURM_commands=['python /home/yamaguchi-s/Desktop/Research/practice/source/gan_analysis/main.py {} {} {} {} {} {} {} {}'.format(
+                    SLURM_commands=['python /home/yamaguchi-s/Desktop/Research/practice/source/gan_analysis/main.py {} {} {} {} {} {} {} {} {} {}'.format(
                                 save_data_path, save_fig_path, step, learning_rate_D, learning_rate_G,
                                 num_cluster, scale, std, z_size, sample_size)]
 
