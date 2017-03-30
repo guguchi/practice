b4fd9001403c03a7708bc231f1f53dd4628778ea
guguchi <docomosoft091@gmail.com>
2017-03-30 23:44:48 +0900

    0330

--- source/gan_analysis/experiment_wasserstein.py
+++ source/gan_analysis/experiment_wasserstein.py
@@ -30,7 +30,7 @@ for learning_rate_D in learning_rate_D_list:
                 save_fig_path = save_fig_root+'d_{}_g_{}_cl_{}_std_{}_z_{}/'.format(
                   learning_rate_D, learning_rate_G, num_cluster, std, z_size)
 
-                SLURM_commands=['python /home/yamaguchi-s/Desktop/Research/practice/source/gan_analysis/main.py {} {} {} {} {} {} {} {} {} {}'.format(
+                SLURM_commands=['python /home/yamaguchi-s/Desktop/Research/practice/source/gan_analysis/main_wasserstein.py {} {} {} {} {} {} {} {} {} {} {}'.format(
                             save_data_path, save_fig_path, g_iteration, d_iteration,
                             learning_rate_D, learning_rate_G,
                             num_cluster, scale, std, z_size, sample_size)]
