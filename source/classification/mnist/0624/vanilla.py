45cf382ad22b6461d904c1b59065b22e46e5a023
guguchi <docomosoft091@gmail.com>
2017-06-29 17:28:10 +0900

    0629

--- source/classification/mnist/0624/vanilla.py
+++ source/classification/mnist/0624/vanilla.py
@@ -41,7 +41,7 @@ import tensorflow as tf
 FLAGS = None
 
 
-def deepnn(x, _dropout):
+def deepnn(x):
     """deepnn builds the graph for a deep net for classifying digits.
     Args:
         x: an input tensor with the dimensions (N_examples, 784), where 784 is the
@@ -56,50 +56,53 @@ def deepnn(x, _dropout):
     # Last dimension is for "features" - there is only one here, since images are
     # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
     x_image = tf.reshape(x, [-1, 28, 28, 1])
-    if _dropout:
-        keep_prob = tf.placeholder(tf.float32)
 
-    # 1 layer
-    W_1 = weight_variable([784, 784])
-    b_1 = bias_variable([784])
-    h_1 = tf.nn.relu(tf.matmul(x_image, W_1) + b_1)
-    if _dropout:
-        h_1 = tf.nn.dropout(h_1, keep_prob)
+    # First convolutional layer - maps one grayscale image to 32 feature maps.
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
-    # 2 layer
-    W_2 = weight_variable([784, 784])
-    b_2 = bias_variable([784])
-    h_2 = tf.nn.relu(tf.matmul(h_1, W_2) + b_2)
-    if _dropout:
-        h_2 = tf.nn.dropout(h_2, keep_prob)
+    # Pooling layer - downsamples by 2X.
+    h_pool1 = max_pool_2x2(h_conv1)
 
-    # 3 layer
-    W_3 = weight_variable([784, 784])
-    b_3 = bias_variable([784])
-    h_3 = tf.nn.relu(tf.matmul(h_2, W_3) + b_3)
-    if _dropout:
-        h_3 = tf.nn.dropout(h_3, keep_prob)
+    # Second convolutional layer -- maps 32 feature maps to 64.
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
-    # 4 layer
-    W_4 = weight_variable([784, 784])
-    b_4 = bias_variable([784])
-    h_4 = tf.nn.relu(tf.matmul(h_3, W_4) + b_4)
-    if _dropout:
-        h_4 = tf.nn.dropout(h_4, keep_prob)
+    # Second pooling layer.
+    h_pool2 = max_pool_2x2(h_conv2)
 
-    # 5 layer
-    W_5 = weight_variable([784, 784])
-    b_5 = bias_variable([784])
-    h_5 = tf.nn.relu(tf.matmul(h_4, W_5) + b_5)
-    if _dropout:
-        h_5 = tf.nn.dropout(h_5, keep_prob)
+    # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
+    # is down to 7x7x64 feature maps -- maps this to 1024 features.
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
 
-    # output
-    W_out = weight_variable([784, 10])
-    b_out = bias_variable([10])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    y_out = tf.matmul(h_5, W_out) + b_out
-    return y_out
+    # Dropout - controls the complexity of the model, prevents co-adaptation of
+    # features.
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    # Map the 1024 features to 10 classes, one for each digit
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    return y_conv, keep_prob
+
+
+def conv2d(x, W):
+    """conv2d returns a 2d convolution layer with full stride."""
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    """max_pool_2x2 downsamples a feature map by 2X."""
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
 
 
 def weight_variable(shape):
@@ -114,7 +117,7 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def main(_dropout):
+def main(_):
     # Import data
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
@@ -125,19 +128,15 @@ def main(_dropout):
     y_ = tf.placeholder(tf.float32, [None, 10])
 
     # Build the graph for the deep net
-    y_out = deepnn(x, _dropout)
+    y_conv, keep_prob = deepnn(x)
 
-    # objective
     cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_out))
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
     train_step = tf.train.AdamOptimizer(0.0001).minimize(cross_entropy)
-
-    # evaluation
-    correct_prediction = tf.equal(tf.argmax(y_out, 1), tf.argmax(y_, 1))
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     test_accuracy_list = []
-    train_accuracy_list = []
     cross_entropy_list = []
     save_data_path = '/home/ishii/Desktop/research/practice/data/classification/vanilla/'
     if not os.path.exists(save_data_path):
@@ -147,27 +146,28 @@ def main(_dropout):
         sess.run(tf.global_variables_initializer())
         for i in range(20000):
             batch = mnist.train.next_batch(50)
-
-            _, cross_entropy_curr, train_accuracy = sess.run([train_step, cross_entropy, accuracy], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-            train_accuracy_list.append(train_accuracy)
-            cross_entropy_list.append(cross_entropy_curr)
-
             if i % 100 == 0:
-                test_accuracy = accuracy.eval(feed_dict={
+                train_accuracy = accuracy.eval(feed_dict={
                     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-                #test_accuracy = accuracy.eval(feed_dict={
+                #train_accuracy = accuracy.eval(feed_dict={
                 #    x: batch[0], y_: batch[1], keep_prob: 1.0})
-                print('step %d, training accuracy %g' % (i, test_accuracy))
-                test_accuracy_list.append(test_accuracy)
+                print('step %d, training accuracy %g' % (i, train_accuracy))
+                test_accuracy_list.append(train_accuracy)
+            _, cross_entropy_curr = sess.run([train_step, cross_entropy], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+            cross_entropy_list.append(cross_entropy_curr)
 
         test_accuracy = accuracy.eval(feed_dict={
             x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
         print('test accuracy %g' % test_accuracy)
         test_accuracy_list.append(test_accuracy)
-
-        np.save(save_data_path+'test_accuracy_vanilla_dropout_{}.npy'.format(_dropout), test_accuracy_list)
-        np.save(save_data_path+'train_accuracy_vanilla_dropout_{}.npy'.format(_dropout), train_accuracy_list)
-        np.save(save_data_path+'cross_entropy_vanilla_dropout_{}.npy'.format(_dropout), cross_entropy_list)
+        plt.plot(np.arange(len(test_accuracy_list)), test_accuracy_list, label='test accuracy')
+        plt.legend()
+        plt.savefig(save_data_path+'test_accuracy_list.png')
+        plt.close()
+        plt.plot(np.arange(len(cross_entropy_list)), cross_entropy_list, label='test accuracy')
+        plt.legend()
+        plt.savefig(save_data_path+'cross_entropy_list.png')
+        plt.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -175,6 +175,4 @@ if __name__ == '__main__':
                         default='/tmp/tensorflow/mnist/input_data',
                         help='Directory for storing input data')
     FLAGS, unparsed = parser.parse_known_args()
-    _dropout = False
-    main = main(_dropout)
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
