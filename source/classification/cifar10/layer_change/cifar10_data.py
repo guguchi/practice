6a88c6fdccb092b878fe61a3d64b6c10097ae385
guguchi <docomosoft091@gmail.com>
2017-07-06 20:43:01 +0900

    0706

---
+++ source/classification/cifar10/layer_change/cifar10_data.py
@@ -0,0 +1,197 @@
+#coding:utf-8
+
+import os
+import sys
+import tarfile
+from six.moves import urllib
+import tensorflow as tf
+
+
+def maybe_download_and_extract(data_dir):
+    """データダウンロード"""
+    dest_directory = data_dir
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename,
+                float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+    extracted_dir_path = os.path.join(dest_directory, 'cifar-10-batches-bin')
+    if not os.path.exists(extracted_dir_path):
+        tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
+def read_cifar10(filename_queue):
+    """Reads and parses examples from CIFAR10 data files."""
+
+    class CIFAR10Record(object):
+        pass
+    result = CIFAR10Record()
+
+    label_bytes = 1
+    result.height = 32
+    result.width = 32
+    result.depth = 3
+    image_bytes = result.height * result.width * result.depth
+    record_bytes = label_bytes + image_bytes
+
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+
+    ##readerにqueueを渡してファイルを開く
+    result.key, value = reader.read(filename_queue)
+
+    # 6. readの結果からデータをdecode
+    record_bytes = tf.decode_raw(value, tf.uint8)
+
+    result.label = tf.cast(
+        tf.strided_slice(record_bytes, [0], [label_bytes]), tf.int32)
+
+    depth_major = tf.reshape(
+        tf.strided_slice(record_bytes, [label_bytes],
+                        [label_bytes + image_bytes]),
+                        [result.depth, result.height, result.width])
+    result.uint8image = tf.transpose(depth_major, [1, 2, 0])
+
+    return result
+
+
+def _generate_image_and_label_batch(image, label, min_queue_examples,
+                                    batch_size, shuffle):
+    """Construct a queued batch of images and labels.
+    Args:
+        image: 3-D Tensor of [height, width, 3] of type.float32.
+        label: 1-D Tensor of type.int32
+        min_queue_examples: int32, minimum number of samples to retain
+        in the queue that provides of batches of examples.
+        batch_size: Number of images per batch.
+        shuffle: boolean indicating whether to use a shuffling queue.
+    Returns:
+        images: Images. 4D tensor of [batch_size, height, width, 3] size.
+        labels: Labels. 1D tensor of [batch_size] size.
+    """
+    # Create a queue that shuffles the examples, and then
+    # read 'batch_size' images + labels from the example queue.
+    num_preprocess_threads = 16
+    if shuffle:
+        images, label_batch = tf.train.shuffle_batch(
+            [image, label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 1 * batch_size,
+            min_after_dequeue=min_queue_examples)
+    else:
+        images, label_batch = tf.train.batch(
+            [image, label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 1 * batch_size)
+
+    label = tf.reshape(label_batch, [batch_size])
+    one_hot_label = tf.one_hot(label, depth = 10)
+
+    images = tf.reshape(images, [batch_size, 32*32])
+
+    return images, one_hot_label
+
+
+def train_input(data_dir, batch_size):
+    filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)
+                 for i in xrange(1, 6)]
+    for f in filenames:
+      if not tf.gfile.Exists(f):
+        raise ValueError('Failed to find file: ' + f)
+
+    # Create a queue that produces the filenames to read.
+    filename_queue = tf.train.string_input_producer(filenames)
+
+    # Read examples from files in the filename queue.
+    read_input = read_cifar10(filename_queue)
+    gray_image = tf.image.rgb_to_grayscale(read_input.uint8image)
+    gray_image = tf.cast(gray_image, tf.float32)
+    float_image = tf.image.per_image_standardization(gray_image)
+
+    float_image.set_shape([32, 32, 1])
+    read_input.label.set_shape([1])
+
+    min_fraction_of_examples_in_queue = 0.4
+    NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *
+                            min_fraction_of_examples_in_queue)
+    print ('Filling queue with %d CIFAR images before starting to train. '
+            'This will take a few minutes.' % min_queue_examples)
+
+    return _generate_image_and_label_batch(float_image, read_input.label,
+                                           min_queue_examples, batch_size,
+                                           shuffle=True)
+
+
+def test_input(data_dir, batch_size):
+    filenames = [os.path.join(data_dir, 'test_batch.bin')]
+
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file: ' + f)
+
+    # Create a queue that produces the filenames to read.
+    filename_queue = tf.train.string_input_producer(filenames)
+
+    # Read examples from files in the filename queue.
+    read_input = read_cifar10(filename_queue)
+    gray_image = tf.image.rgb_to_grayscale(read_input.uint8image)
+    gray_image = tf.cast(gray_image, tf.float32)
+    float_image = tf.image.per_image_standardization(gray_image)
+
+    float_image.set_shape([32, 32, 1])
+    read_input.label.set_shape([1])
+
+    # Ensure that the random shuffling has good mixing properties.
+    min_fraction_of_examples_in_queue = 0.4
+    NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_EVAL *
+                           min_fraction_of_examples_in_queue)
+
+    # Generate a batch of images and labels by building up a queue of examples.
+    return _generate_image_and_label_batch(float_image, read_input.label,
+                                            min_queue_examples, batch_size,
+                                            shuffle=False)
+
+"""
+if __name__ == '__main__':
+
+    data_dir = './'
+    batch_size = 32
+    max_steps = 10
+    maybe_download_and_extract(data_dir)
+
+    images, labels = train_input(data_dir + 'cifar-10-batches-bin/', batch_size)
+
+    # 8. 実行
+    sess = tf.Session()
+    tf.train.start_queue_runners(sess=sess)
+    for step in xrange(max_steps):
+        img, label = sess.run([images, labels])
+        print(label)
+    print("FIN.")
+    img = np.reshape(img, [batch_size, 32, 32])
+    print img[0]
+    #plt.gray()
+    plt.subplot(151)
+    plt.imshow(img[0], interpolation='nearest')
+    plt.subplot(152)
+    plt.imshow(img[1], interpolation='nearest')
+    plt.subplot(153)
+    plt.imshow(img[2], interpolation='nearest')
+    plt.subplot(154)
+    plt.imshow(img[3], interpolation='nearest')
+    plt.subplot(155)
+    plt.imshow(img[4], interpolation='nearest')
+    plt.show()
+"""
---
+++ source/classification/cifar10/layer_change/vanilla_relu.py
@@ -0,0 +1,325 @@
+#coding:utf-8
+import argparse
+import sys
+import os
+import math
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import tensorflow as tf
+from cifar10_data import *
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_float('learning_rate', 0.0005, "学習率")
+tf.app.flags.DEFINE_integer('iteration', 3, "学習反復回数")
+tf.app.flags.DEFINE_integer('step', 500000, "学習数")
+tf.app.flags.DEFINE_integer('batch_size', 50, "バッチサイズ")
+tf.app.flags.DEFINE_integer('layer_size', 5, "レイヤー数")
+tf.app.flags.DEFINE_integer('test_batch_size', 1000, "テストバッチサイズ")
+tf.app.flags.DEFINE_float('gpu_memory', 0.1, "gpuメモリ使用割合")
+tf.app.flags.DEFINE_integer('test_example', 10000, "テストデータ数")
+tf.app.flags.DEFINE_string('cifar_data_dir', './../../../data/', "cifar10保存先")
+tf.app.flags.DEFINE_string('save_data_path', './../../../data/classification/cifar10/vanilla_batch/', "データ保存先")
+
+
+def deepnn(x):
+    # 1 layer
+    W_1 = weight_variable([32*32, 32*32])
+    b_1 = bias_variable([32*32])
+    h_1 = tf.nn.relu(tf.matmul(x, W_1) + b_1)
+    jacobian = W_1
+
+    if FLAGS.layer_size == 1:
+        # output
+        W_out = weight_variable([32*32, 10])
+        b_out = bias_variable([10])
+
+        y_out = tf.matmul(h_1, W_out) + b_out
+        entropy_all = compute_entropy_with_svd(jacobian)
+        return y_out, entropy_all, h_1
+
+    # 2 layer
+    W_2 = weight_variable([32*32, 32*32])
+    b_2 = bias_variable([32*32])
+    h_2 = tf.nn.relu(tf.matmul(h_1, W_2) + b_2)
+    jacobian = tf.matmul(W_2, jacobian)
+
+    if FLAGS.layer_size == 2:
+        # output
+        W_out = weight_variable([32*32, 10])
+        b_out = bias_variable([10])
+
+        y_out = tf.matmul(h_2, W_out) + b_out
+        entropy_all = compute_entropy_with_svd(jacobian)
+        return y_out, entropy_all, h_2
+
+    # 3 layer
+    W_3 = weight_variable([32*32, 32*32])
+    b_3 = bias_variable([32*32])
+    h_3 = tf.nn.relu(tf.matmul(h_2, W_3) + b_3)
+    jacobian = tf.matmul(W_3, jacobian)
+
+    if FLAGS.layer_size == 3:
+        # output
+        W_out = weight_variable([32*32, 10])
+        b_out = bias_variable([10])
+
+        y_out = tf.matmul(h_3, W_out) + b_out
+        entropy_all = compute_entropy_with_svd(jacobian)
+        return y_out, entropy_all, h_3
+
+    # 4 layer
+    W_4 = weight_variable([32*32, 32*32])
+    b_4 = bias_variable([32*32])
+    h_4 = tf.nn.relu(tf.matmul(h_3, W_4) + b_4)
+    jacobian = tf.matmul(W_4, jacobian)
+
+    if FLAGS.layer_size == 4:
+        # output
+        W_out = weight_variable([32*32, 10])
+        b_out = bias_variable([10])
+
+        y_out = tf.matmul(h_4, W_out) + b_out
+        entropy_all = compute_entropy_with_svd(jacobian)
+        return y_out, entropy_all, h_4
+
+    # 5 layer
+    W_5 = weight_variable([32*32, 32*32])
+    b_5 = bias_variable([32*32])
+    h_5 = tf.nn.relu(tf.matmul(h_4, W_5) + b_5)
+    jacobian = tf.matmul(W_5, jacobian)
+
+    if FLAGS.layer_size == 5:
+        # output
+        W_out = weight_variable([32*32, 10])
+        b_out = bias_variable([10])
+
+        y_out = tf.matmul(h_5, W_out) + b_out
+        entropy_all = compute_entropy_with_svd(jacobian)
+        return y_out, entropy_all, h_5
+
+    # 6 layer
+    W_6 = weight_variable([32*32, 32*32])
+    b_6 = bias_variable([32*32])
+    h_6 = tf.nn.relu(tf.matmul(h_5, W_6) + b_6)
+    jacobian = tf.matmul(W_6, jacobian)
+
+    if FLAGS.layer_size == 6:
+        # output
+        W_out = weight_variable([32*32, 10])
+        b_out = bias_variable([10])
+
+        y_out = tf.matmul(h_6, W_out) + b_out
+        entropy_all = compute_entropy_with_svd(jacobian)
+        return y_out, entropy_all, h_6
+
+    # 7 layer
+    W_7 = weight_variable([32*32, 32*32])
+    b_7 = bias_variable([32*32])
+    h_7 = tf.nn.relu(tf.matmul(h_6, W_7) + b_7)
+    jacobian = tf.matmul(W_7, jacobian)
+
+    # output
+    W_out = weight_variable([32*32, 10])
+    b_out = bias_variable([10])
+
+    y_out = tf.matmul(h_7, W_out) + b_out
+    entropy_all = compute_entropy_with_svd(jacobian)
+    return y_out, entropy_all, h_7
+
+
+def weight_variable(shape):
+    """weight_variable generates a weight variable of a given shape."""
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    """bias_variable generates a bias variable of a given shape."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def svd(A, full_matrices=False, compute_uv=True, name=None):
+    # since dA = dUSVt + UdSVt + USdVt
+    # we can simply recompute each matrix using A = USVt
+    # while blocking gradients to the original op.
+    M, N = A.get_shape().as_list()
+    P = min(M, N)
+    S0, U0, V0 = map(tf.stop_gradient, tf.svd(A, full_matrices=True, name=name))
+    #Ui, Vti = map(tf.matrix_inverse, [U0, tf.transpose(V0, (0, 2, 1))])
+    Ui = tf.transpose(U0)
+    Vti = V0
+    # A = USVt
+    # S = UiAVti
+    S = tf.matmul(Ui, tf.matmul(A, Vti))
+    S = tf.matrix_diag_part(S)
+    return S
+
+
+def compute_entropy_with_svd(jacobian):
+    with tf.device('/cpu:0'):
+        s = svd(jacobian, compute_uv=False)
+    #if self.layer_method == "each":
+    s = tf.maximum(s, 0.1 ** 8)
+    log_determine = tf.log(tf.abs(s))
+    entropy = tf.reduce_mean(log_determine)
+    return entropy
+
+
+def plot(samples, layer_samples):
+    fig = plt.figure(figsize=(6, 6))
+    gs = gridspec.GridSpec(6, 6)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for (i, sample) in enumerate(samples):
+        ax = plt.subplot(gs[2*i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.reshape(32, 32), cmap='Greys_r')
+
+        ax = plt.subplot(gs[2*i + 1])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(layer_samples[i].reshape(32, 32), cmap='Greys_r')
+
+    return fig
+
+
+def main(argv):
+    # data preparation
+    maybe_download_and_extract(FLAGS.cifar_data_dir)
+    num_iter = 10.0#int(math.ceil(FLAGS.test_example / FLAGS.test_batch_size))
+
+    # data load
+    train_images_batch, trains_labels_batch = train_input(FLAGS.cifar_data_dir + 'cifar-10-batches-bin/',
+                                              FLAGS.batch_size)
+    #train_images, train_labels = train_input(FLAGS.cifar_data_dir + 'cifar-10-batches-bin/',
+    #                                      FLAGS.test_batch_size)
+    test_images, test_labels = test_input(FLAGS.cifar_data_dir + 'cifar-10-batches-bin/',
+                                          FLAGS.test_batch_size)
+
+    # placeholder
+    x = tf.placeholder(tf.float32, [None, 32*32])
+    y_ = tf.placeholder(tf.float32, [None, 10])
+
+    # model
+    y_out, entropy_all, h_7 = deepnn(x)
+
+    # objective
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_out))
+    train_step = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(cross_entropy)
+    #GradientDescentOptimizer / AdamOptimizer / MomentumOptimizer
+
+    # evaluation
+    correct_prediction = tf.equal(tf.argmax(y_out, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # save data
+    train_accuracy_list_batch = np.zeros((FLAGS.iteration, FLAGS.step), dtype=np.float32)
+    #train_accuracy_list = np.zeros((FLAGS.iteration, FLAGS.step), dtype=np.float32)
+    test_accuracy_list = np.zeros((FLAGS.iteration, FLAGS.step), dtype=np.float32)
+    cross_entropy_list = np.zeros((FLAGS.iteration, FLAGS.step), dtype=np.float32)
+    entropy_list = np.zeros((FLAGS.iteration, FLAGS.step), dtype=np.float32)
+
+    save_path = FLAGS.save_data_path + 'batch_{}_alpha_{}/'.format(FLAGS.batch_size, FLAGS.learning_rate)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    config = tf.ConfigProto(
+             gpu_options=tf.GPUOptions(
+             per_process_gpu_memory_fraction=FLAGS.gpu_memory # 最大値の50%まで
+             )
+    )
+    config_cpu = tf.ConfigProto(
+        device_count = {'GPU': 0}
+    )
+
+    sess_train_example_batch = tf.Session(config=config_cpu)
+    tf.train.start_queue_runners(sess=sess_train_example_batch)
+    #sess_train_example = tf.Session(config=config_cpu)
+    #tf.train.start_queue_runners(sess=sess_train_example)
+    sess_test_example = tf.Session(config=config_cpu)
+    tf.train.start_queue_runners(sess=sess_test_example)
+
+    for _iter in range(FLAGS.iteration):
+        print '=== iteration {} ==='.format(_iter)
+
+        with tf.Session(config=config) as sess:
+            sess.run(tf.global_variables_initializer())
+
+            for i in range(FLAGS.step):
+
+                _train_images_batch, _trains_labels_batch = sess_train_example_batch.run([train_images_batch, trains_labels_batch])
+
+                _, cross_entropy_curr, train_accuracy_batch = sess.run(
+                    [train_step, cross_entropy, accuracy],
+                    feed_dict={x: _train_images_batch, y_: _trains_labels_batch})
+
+                train_accuracy_list_batch[_iter][i] = train_accuracy_batch
+                cross_entropy_list[_iter][i] = cross_entropy_curr
+
+                if i % 5000 == 0 or i == FLAGS.step - 1:
+                    """
+                    true_train_accuracy = 0.0
+                    _step = 0
+                    while _step < num_iter:
+                        _train_images, _trains_labels = sess_train_example.run([train_images, train_labels])
+
+                        train_accuracy = accuracy.eval(feed_dict={
+                            x: _train_images, y_: _trains_labels})
+                        true_train_accuracy += train_accuracy
+                        _step += 1
+                    true_train_accuracy = true_train_accuracy / num_iter
+                    print('step %d, test accuracy %g' % (i, true_train_accuracy))
+                    train_accuracy_list[_iter][i] = true_train_accuracy
+                    """
+                    true_test_accuracy = 0.0
+                    _step = 0
+                    while _step < num_iter:
+                        _test_images, _test_labels = sess_test_example.run([test_images, test_labels])
+
+                        test_accuracy = accuracy.eval(feed_dict={
+                            x: _test_images, y_: _test_labels})
+                        true_test_accuracy += test_accuracy
+                        _step += 1
+                    true_test_accuracy = true_test_accuracy / num_iter
+                    test_accuracy_list[_iter][i] = true_test_accuracy
+
+                    #_test_images, _test_labels = sess_test_example.run([test_images, test_labels])
+
+                    entropy_curr = entropy_all.eval(feed_dict={
+                        x: _test_images})
+
+                    entropy_list[_iter][i] = entropy_curr
+                    #entropy_curr = 1.0
+                    print('step %d, test accuracy %g, entropy %g' % (i, true_test_accuracy, entropy_curr))
+                    print '---------------------'
+
+                if _iter == 0 and (i % 5000 == 0 or i == FLAGS.step - 1):
+                    _test_images, _test_labels = sess_test_example.run([test_images, test_labels])
+
+                    layer_samples = sess.run([h_7],
+                                  feed_dict={x: _test_images[:18]})
+                    fig = plot(_test_images[:18], layer_samples[0])
+                    plt.savefig(save_path+'layer_samples_{}.png'.format(i))
+                    plt.close()
+
+
+    #np.save(save_path+'train_accuracy.npy', train_accuracy_list)
+    np.save(save_path+'train_accuracy_batch.npy', train_accuracy_list_batch)
+    np.save(save_path+'test_accuracy.npy', test_accuracy_list)
+    np.save(save_path+'cross_entropy.npy', cross_entropy_list)
+    np.save(save_path+'entropy.npy', entropy_list)
+
+
+if __name__ == '__main__':
+
+    tf.app.run(main=main)
