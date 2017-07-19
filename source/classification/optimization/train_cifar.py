a0c881b48b6013d4ca3eddca15cd409304e51a4f
guguchi <docomosoft091@gmail.com>
2017-07-19 19:56:10 +0900

    0719

--- source/classification/optimization/train_cifar.py
+++ source/classification/optimization/train_cifar.py
@@ -12,12 +12,42 @@ from chainer.datasets import get_cifar100
 import models.VGG
 
 
+def compute_classification_accuracy(model, x, t):
+	xp = model.xp
+	batches = xp.split(x, len(x) // 100)
+	scores = None
+	for batch in batches:
+		p = F.softmax(model(batch, apply_softmax=False)).data
+		scores = p if scores is None else xp.concatenate((scores, p), axis=0)
+	return float(F.accuracy(scores, Variable(t)).data)
+
+
+def cache_weights(model):
+	cached_weights = {}
+	for name, param in model.namedparams():
+		with cuda.get_device(param.data):
+			xp = cuda.get_array_module(param.data)
+			cached_weights[name] = xp.copy(param.data)
+
+
+def restore_weights(model, cached_weights):
+	for name, param in model.namedparams():
+		with cuda.get_device(param.data):
+			if name not in cached_weights:
+				raise Exception()
+			param.data = cached_weights[name]
+
+
 def main():
     parser = argparse.ArgumentParser(description='Chainer CIFAR example:')
     parser.add_argument('--dataset', '-d', default='cifar10',
                         help='The dataset to use: cifar10 or cifar100')
     parser.add_argument('--batchsize', '-b', type=int, default=64,
                         help='Number of images in each mini-batch')
+    parser.add_argument('--minibatchsize', '-b', type=int, default=16,
+                        help='Number of images in each mini-mini-batch')
+    parser.add_argument('--valid', '-b', type=int, default=10,
+                        help='Number of validation in each mini-batch')
     parser.add_argument('--learnrate', '-l', type=float, default=0.05,
                         help='Learning rate for SGD')
     parser.add_argument('--epoch', '-e', type=int, default=300,
@@ -41,14 +71,14 @@ def main():
     if args.dataset == 'cifar10':
         print('Using CIFAR10 dataset.')
         class_labels = 10
-        train, test = get_cifar10()
+        cifar_train, cifar_test = get_cifar10()
     elif args.dataset == 'cifar100':
         print('Using CIFAR100 dataset.')
         class_labels = 100
-        train, test = get_cifar100()
+        cifar_train, cifar_test = get_cifar100()
     else:
         raise RuntimeError('Invalid dataset choice.')
-    model = L.Classifier(models.VGG.VGG(class_labels))
+    model = models.VGG.VGG(class_labels)
     if args.gpu >= 0:
         # Make a specified GPU current
         chainer.cuda.get_device_from_id(args.gpu).use()
@@ -58,48 +88,82 @@ def main():
     optimizer.setup(model)
     optimizer.add_hook(chainer.optimizer.WeightDecay(5e-4))
 
-    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
-                                                 repeat=False, shuffle=False)
-    # Set up a trainer
-    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
-    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+    train_data, train_label = cifar_train
+	test_data, test_label = cifar_test
+	if args.gpu_device >= 0:
+		train_data = cuda.to_gpu(train_data)
+		train_label = cuda.to_gpu(train_label)
+		test_data = cuda.to_gpu(test_data)
+		test_label = cuda.to_gpu(test_label)
+	train_loop = len(train_data) // args.minibatchsize
+	train_indices = np.arange(len(train_data))
 
-    # Evaluate the model with the test dataset for each epoch
-    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+    # training cycle
+	for epoch in xrange(1, args.epoch):
+		np.random.shuffle(train_indices)	# shuffle data
+		sum_loss = 0
 
-    # Reduce the learning rate by half every 25 epochs.
-    trainer.extend(extensions.ExponentialShift('lr', 0.5),
-                   trigger=(25, 'epoch'))
+        with chainer.using_config("train", True):
 
-    # Dump a computational graph from 'loss' variable at the first iteration
-    # The "main" refers to the target link of the "main" optimizer.
-    trainer.extend(extensions.dump_graph('main/loss'))
+            # loop over all batches
+			for itr in xrange(1, train_loop + 1):
+                # sample minibatch
+				batch_range = np.arange(itr * args.batchsize, min((itr + 1) * args.batchsize, len(train_data)))
+				x = train_data[train_indices[batch_range]]
+				t = train_label[train_indices[batch_range]]
 
-    # Take a snapshot at each epoch
-    trainer.extend(extensions.snapshot(), trigger=(args.epoch, 'epoch'))
+                init_weights = cache_weights(model)
+                weight_list = []
+                valid_accuracy = np.zeros(args.valid, dtype=np.float32)
+                valid_loss = np.zeros(args.valid, dtype=np.float32)
 
-    # Write a log of evaluation statistics for each epoch
-    trainer.extend(extensions.LogReport())
+                for valid_iter in xrange(args.valid):
 
-    # Print selected entries of the log to stdout
-    # Here "main" refers to the target link of the "main" optimizer again, and
-    # "validation" refers to the default name of the Evaluator extension.
-    # Entries other than 'epoch' are reported by the Classifier link, called by
-    # either the updater or the evaluator.
-    trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'validation/main/loss',
-         'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
+                    restore_weights(model, init_weights)
 
-    # Print a progress bar to stdout
-    trainer.extend(extensions.ProgressBar())
+                    train_indices = np.random.choice(len(x), args.minibatchsize)
+                    valid_indices = np.ones(10, dtype=bool)
+                    valid_indices[train_indices] = False
 
-    if args.resume:
-        # Resume from a snapshot
-        chainer.serializers.load_npz(args.resume, trainer)
+                    x_train = x[train_indices]
+                    t_train = t[train_indices]
+                    x_valid = x[valid_indices]
+                    t_valid = t[valid_indices]
 
-    # Run the training
-    trainer.run()
+                    # to gpu
+    				if model.xp is cuda.cupy:
+    					x_train = cuda.to_gpu(x_train)
+    					t_train = cuda.to_gpu(t_train)
+                        x_valid = cuda.to_gpu(x_valid)
+    					t_valid = cuda.to_gpu(t_valid)
+
+                    logits = model(x_train)
+    				loss = F.softmax_cross_entropy(logits, Variable(t_train))
+
+                    # update weights
+    				optimizer.update(lossfun=lambda: loss)
+
+                    accuracy_valid = compute_classification_accuracy(
+                        model, x_valid, t_valid)
+
+                    update_weights = cache_weights(model)
+                    weight_list.append(update_weights)
+                    valid_accuracy[valid_iter] = accuracy_valid
+                    valid_loss[valid_iter] = float(loss.data)
+
+                best_index = np.argmax(valid_accuracy)
+                best_weight = weight_list[best_index]
+                best_loss = valid_loss[best_index]
+                restore_weights(model, best_weight)
+
+                sum_loss += best_loss
+
+        with chainer.using_config("train", False):
+			accuracy_train = compute_classification_accuracy(model, train_data, train_label)
+			accuracy_test = compute_classification_accuracy(model, test_data, test_label)
+
+        sys.stdout.write("\r\033[2KEpoch {} - loss: {:.8f} - acc: {:.5f} (train), {:.5f} (test)\n".format(epoch, sum_loss / train_loop, accuracy_train, accuracy_test))
+		sys.stdout.flush()
 
 
 if __name__ == '__main__':
